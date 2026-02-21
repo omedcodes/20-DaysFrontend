781d7b1da1addb234fd230f3741b785b7add0ab0
omedcodes <omedsafi21@gmail.com>
2026-02-21 19:29:11 +0100

    Implement generatePalette function and random Color gen function

--- color-palette-generator/index.js
+++ color-palette-generator/index.js
@@ -35,3 +35,23 @@ function showCopySuccess(element) {
     element.style.color = "";
   }, 1500);
 }
+
+function generatePalette() {
+  const colors = [];
+
+  for (let i = 0; i < 5; i++) {
+    colors.push(generateRandomColor());
+  }
+
+  updatePaletteDisplay(colors);
+}
+
+function generateRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
